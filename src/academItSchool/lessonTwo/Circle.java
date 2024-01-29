ce9509f2189dc7d65a38cb389679a080533c9367
Valery0904 <valery.tugolukov@gmail.com>
2024-01-30 01:32:36 +0700

    2

---
+++ src/academItSchool/lessonTwo/Circle.java
@@ -0,0 +1,21 @@
+package academItSchool.lessonTwo;
+
+public class Circle {
+    public static void main(String[] args) {
+        double radius = 11;
+        double circleArea = Math.PI * Math.pow(radius, 2);
+        double circumference = 2 * Math.PI * radius;
+
+        System.out.println("Площадь круга = " + circleArea);
+        System.out.println("Длина окружности = " + circumference);
+
+        double circleRadius = Math.sqrt(circleArea / Math.PI);
+
+        System.out.println("Радиус круга = " + circleRadius);
+
+        int angleDegrees = 180;
+        double sectorArea = Math.PI * Math.pow(circleRadius, 2) * angleDegrees / 360;
+
+        System.out.println("Площадь сектора круга = " + sectorArea);
+    }
+}
---
+++ src/academItSchool/lessonTwo/Rectangle.java
@@ -0,0 +1,23 @@
+package academItSchool.lessonTwo;
+
+import java.util.Scanner;
+
+public class Rectangle {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Ведите длину прямоугольника:");
+        double rectangleLength = scanner.nextDouble();
+
+        System.out.println("Ведите ширину прямоугольника:");
+        double rectangleWidth = scanner.nextDouble();
+
+        double rectangleArea = rectangleLength * rectangleWidth;
+        double rectanglePerimeter = 2 * (rectangleLength + rectangleWidth);
+
+        System.out.printf("Длина = %.2f%n", rectangleLength);
+        System.out.printf("Ширина = %.2f%n", rectangleWidth);
+        System.out.printf("Площадь прямоугольника = %.2f%n", rectangleArea);
+        System.out.printf("Периметр прямоугольника = %.2f", rectanglePerimeter);
+    }
+}
