f59e798f0a1b025e579f969cbb8aeb0d183f3062
Valery0904 <valery.tugolukov@gmail.com>
2024-01-30 00:19:46 +0700

    2

---
+++ src/academItSchool/lessonOne/Calculation.java
@@ -0,0 +1,11 @@
+package academItSchool.lessonOne;
+
+public class Calculation {
+    public static void main(String[] args) {
+        int a = 3;
+        int b = 2;
+
+        System.out.println(2 * a + 2 * b - 3 * a * b * (a + b));
+        System.out.println((2 * a + 2 * b) + (2 * a * 2 * b) - a * b * (a + b));
+    }
+}
