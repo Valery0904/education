2ff457b3653bf12f55b8d9a9072f3cd72c09a2be
Valery0904 <valery.tugolukov@gmail.com>
2024-02-12 22:04:18 +0700

    Задача на курс «Площадь треугольника» • Прочитать с консоли координаты трёх точек на плоскости: (x1, y1), (x2, y2), (x3, y3) • Вычислить и вывести на экран площадь данного треугольника • Для вычисления площади можно воспользоваться формулой Герона: • 𝑆∆ = 𝑝(𝑝 − 𝑎)(𝑝 − 𝑏)(𝑝 − 𝑐), где 𝑝 −полупериметр треугольника 𝑝=(𝑎+𝑏+𝑐)/2, 𝑎, 𝑏, 𝑐 −длины сторон треугольника • Проверить на случай, когда эти точки лежат на одной прямой – в этом случае вычислять площадь не нужно, а нужно вывести сообщение об этом • Для вычисления корня использовать команду Math.sqrt(значение)

---
+++ src/main/java/AcademItSchool/Coursework/TriangleArea.java
@@ -0,0 +1,42 @@
+package AcademItSchool.Coursework;
+
+import java.util.Scanner;
+
+public class TriangleArea {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Введите координату х1 для точки А: ");
+        double x1 = scanner.nextDouble();
+        System.out.print("Введите координату y1 для точки A: ");
+        double y1 = scanner.nextDouble();
+
+        System.out.print("Введите координату x2 для точки B: ");
+        double x2 = scanner.nextDouble();
+        System.out.print("Введите координату y2 для точки B: ");
+        double y2 = scanner.nextDouble();
+
+        System.out.print("Введите координату x3 для точки C: ");
+        double x3 = scanner.nextDouble();
+        System.out.print("Введите координату y3 для точки C: ");
+        double y3 = scanner.nextDouble();
+
+        double epsilon = 1.0e-10;
+
+        if (Math.abs((x2 - x1) * (y3 - y1) - (y2 - y1) * (x3 - x1)) <= epsilon) {
+            System.out.println("Points lie on one straight line");
+        } else {
+            double cutAbLength = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+            double cutBcLength = Math.sqrt(Math.pow(x3 - x2, 2) + Math.pow(y3 - y2, 2));
+            double cutCaLength = Math.sqrt(Math.pow(x1 - x3, 2) + Math.pow(y1 - y3, 2));
+
+            double halfPerimeter = (cutAbLength + cutBcLength + cutCaLength) / 2;
+            double triangleArea = Math.sqrt(halfPerimeter * (halfPerimeter - cutAbLength) * (halfPerimeter -
+                    cutBcLength) * (halfPerimeter - cutCaLength));
+
+            System.out.printf("Длина отрезка AB = %.2f%nДлина отрезка BC = %.2f%nДлина отрезка CA = %.2f%n", cutAbLength,
+                    cutBcLength, cutCaLength);
+            System.out.printf("Площадь треугольника = %.2f", triangleArea);
+        }
+    }
+}
