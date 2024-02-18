e71dcf657137aff8d2531ce513e1bc589cf25601
Valery0904 <valery.tugolukov@gmail.com>
2024-02-18 21:52:38 +0700

    «Простые числа»

---
+++ src/main/java/AcademItSchool/Lesson5/PrimeNumber.java
@@ -0,0 +1,29 @@
+package AcademItSchool.Lesson5;
+
+import java.util.Scanner;
+
+public class PrimeNumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Введите число: ");
+        int number = scanner.nextInt();
+
+        System.out.println("Простые числа не превышающие число " + number + ": ");
+
+        for (int i = 2; i <= number; i++) {
+            boolean isPrime = true;
+
+            for (int j = 2; j < i; j++) {
+                if (i % j == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+
+            if (isPrime) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+}
