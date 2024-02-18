7079fe8909be2ed3c52fb3b45e1a111a6f8cc1d1
Valery0904 <valery.tugolukov@gmail.com>
2024-02-18 20:51:03 +0700

    «Среднее арифметическое» ArithmeticMeanDoWhile: 1. Лучше добавить пустые строки для читаемости: http://joxi.ru/xAeel90h6l7XOA 2. Не исправлен общий п.3 3. Лучше убрать пустые строки, т.к. код связан по смыслу: http://joxi.ru/DrlOPYVin35yoA

--- src/main/java/AcademItSchool/Lesson4/ArithmeticMeanDoWhile.java
+++ src/main/java/AcademItSchool/Lesson4/ArithmeticMeanDoWhile.java
@@ -13,35 +13,28 @@ public class ArithmeticMeanDoWhile {
         int endNumber = scanner.nextInt();
 
         int i = startNumber;
+
         int sum = 0;
         int count = 0;
 
+        int evenNumbersSum = 0;
+        int evenNumbersCount = 0;
+
         do {
+            if (i % 2 == 0) {
+                evenNumbersSum += i;
+                evenNumbersCount++;
+            }
+
             sum += i;
             ++count;
             ++i;
         } while (i <= endNumber);
 
         double average = (double) sum / count;
-
-        System.out.println("Среднее арифметическое всех чисел = " + average);
-
-        // Вычисляем сумму четных чисел.
-        int j = startNumber;
-        int evenNumbersSum = 0;
-        int evenNumbersCount = 0;
-
-        do {
-            if (j % 2 == 0) {
-                evenNumbersSum += j;
-                evenNumbersCount++;
-            }
-
-            j++;
-        } while (j <= endNumber);
-
         double evenNumbersAverage = (double) evenNumbersSum / evenNumbersCount;
 
+        System.out.println("Среднее арифметическое всех чисел = " + average);
         System.out.println("Среднее арифметическое четных чисел = " + evenNumbersAverage);
     }
 }
