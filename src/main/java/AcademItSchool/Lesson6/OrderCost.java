cda64ed2f028a2b22f112281e3b01040e4a087e0
Valery0904 <valery.tugolukov@gmail.com>
2024-02-20 20:44:18 +0700

    «Стоимость заказа» 1. Не должно быть пустых строк сразу после {
    2. Есть ошибки в коде чтения с консоли
    
    3. maxDiscount, minDiscount - переменные объявлены рано
    
    4. В конце текста в println не нужно ставить пробел
    
    5. Лучше добавить пустую строку для читаемости:
    http://joxi.ru/823je7pTK5pERm
    
    6. product1Amount, products1Amount и некоторые др. - не единообразные имена

--- src/main/java/AcademItSchool/Lesson6/OrderCost.java
+++ src/main/java/AcademItSchool/Lesson6/OrderCost.java
@@ -3,15 +3,15 @@ package AcademItSchool.Lesson6;
 import java.util.Scanner;
 
 public class OrderCost {
-
     public static double getOrderCost(int product1Amount, double product1Price, int product2Amount, double product2Price) {
-        final double maxDiscount = 0.9;
-        final double minDiscount = 0.95;
         final int productsAmountForDiscount = 10;
         final int orderCostForDiscount = 1000;
 
         int productsTotalAmount = product1Amount + product2Amount;
         double orderCost = (product1Amount * product1Price) + (product2Amount * product2Price);
+
+        final double maxDiscount = 0.9;
+        final double minDiscount = 0.95;
 
         if (productsTotalAmount >= productsAmountForDiscount && orderCost >= orderCostForDiscount) {
             return orderCost * maxDiscount;
@@ -27,19 +27,19 @@ public class OrderCost {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Введите количество товара №1: ");
-        int products1Amount = scanner.nextInt();
+        System.out.println("Введите количество товара №1:");
+        int product1Amount = scanner.nextInt();
 
-        System.out.println("Введите стоимость единицы товара №1: ");
-        double product1Price = scanner.nextInt();
+        System.out.println("Введите стоимость единицы товара №1:");
+        double product1Price = scanner.nextDouble();
 
-        System.out.println("Введите количество товара №2: ");
-        int products2Amount = scanner.nextInt();
+        System.out.println("Введите количество товара №2:");
+        int product2Amount = scanner.nextInt();
 
-        System.out.println("Введите стоимость единицы товара №2: ");
-        double product2Price = scanner.nextInt();
+        System.out.println("Введите стоимость единицы товара №2:");
+        double product2Price = scanner.nextDouble();
 
-        double orderCost = getOrderCost(products1Amount, product1Price, products2Amount, product2Price);
+        double orderCost = getOrderCost(product1Amount, product1Price, product2Amount, product2Price);
         System.out.println("Стоимость вашего заказа составляет " + orderCost + " руб.");
     }
 }
