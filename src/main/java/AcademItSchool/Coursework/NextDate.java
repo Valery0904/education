cde2d8334e5a1effc9daaa782e5a62e254fa48df
Valery0904 <valery.tugolukov@gmail.com>
2024-02-25 23:31:44 +0700

    Курсовая «Следующая дата»
    1. Для практики выведите дату в формате дд.мм.гггг
    
    2. Сейчас работает для некоторых некорректных дат
    
    3. Лучше убрать пустую строку:
    http://joxi.ru/E2pk5PgiVYJwLm

--- src/main/java/AcademItSchool/Coursework/NextDate.java
+++ src/main/java/AcademItSchool/Coursework/NextDate.java
@@ -16,20 +16,21 @@ public class NextDate {
         int year = scanner.nextInt();
 
         int daysInMonthAmount = 31;
-
-        if ((day < 1 || day > daysInMonthAmount) || (month < 1 || month > 12) || year < 1) {
-            System.out.println("Введенной даты не существует!");
-            return;
-        }
+        boolean isLeapYear = year % 400 == 0 || (year % 4 == 0 && year % 100 != 0);
 
         if (month == 4 || month == 6 || month == 9 || month == 11) {
             daysInMonthAmount = 30;
         } else if (month == 2) {
-            if (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0)) {
+            if (isLeapYear) {
                 daysInMonthAmount = 29;
             } else {
                 daysInMonthAmount = 28;
             }
+        }
+
+        if ((day < 1 || day > daysInMonthAmount) || (month < 1 || month > 12) || year < 1) {
+            System.out.println("Введенной даты не существует!");
+            return;
         }
 
         int nextDay = day + 1;
@@ -46,6 +47,6 @@ public class NextDate {
             nextYear++;
         }
 
-        System.out.println("Следующая дата: " + nextDay + "." + nextMonth + "." + nextYear);
+        System.out.printf("Следующая дата: %02d.%02d.%d", nextDay, nextMonth, nextYear);
     }
 }
