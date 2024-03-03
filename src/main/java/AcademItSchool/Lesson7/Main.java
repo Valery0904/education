7b2263308e0361175b991f6f9911f0126863a630
Valery0904 <valery.tugolukov@gmail.com>
2024-03-03 22:21:58 +0700

    Range
    1. Не должно быть пустых строк сразу после {
    
    2. В текущей версии:
    Range() - обращение через this нужно использовать только если есть конфликт имен
    
    3. main нужно вынести в отдельный класс
    
    4. "числовго" - опечатка
    
    5. В классе Range нужно сделать конструктор, который заполняет поля переданными данными
    
    6. Лучше добавить пустую строку для читаемости:
    http://joxi.ru/Q2KxdK7UX3LdEA
    
    7. В классе Range не нужен конструктор без аргументов, т.к. такой диапазон получается некорректным
    
    8. Есть warning'и
    
    9. epsilon:
    - нужно сделать приватным полем
    - нужно сделать static, чтобы это поле не хранилось во всех объектах Range
    - с учетом предыдущего подпункта нужно будет использовать соглашение именования, при котором все буквы заглавные, а слова разделяются _
    
    10. x - лучше дать полное информативное имя
    
    11. Лучше делать переменные для данных, читаемых с консоли

---
+++ src/main/java/AcademItSchool/Lesson7/Main.java
@@ -0,0 +1,28 @@
+package AcademItSchool.Lesson7;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Введите начало числового диапазона: ");
+        double from = scanner.nextDouble();
+
+        System.out.print("Введите конец числового диапазона: ");
+        double to = scanner.nextDouble();
+
+        System.out.print("Введите число которое хотите проверить: ");
+        double number = scanner.nextDouble();
+
+        Range range = new Range(from, to);
+
+        System.out.printf("Длина числового диапазона от %.2f до %.2f = %.2f%n", from, to, range.getLength());
+
+        if (range.isInside(number)) {
+            System.out.printf("Число %.2f принадлежит диапазону", number);
+        } else {
+            System.out.printf("Число %.2f не принадлежит диапазону", number);
+        }
+    }
+}
--- src/main/java/AcademItSchool/Lesson7/Range.java
+++ src/main/java/AcademItSchool/Lesson7/Range.java
@@ -1,36 +1,11 @@
 package AcademItSchool.Lesson7;
 
-import java.util.Scanner;
-
 public class Range {
+    private static final double EPSILON = 1.0e-10;
+    double from;
+    double to;
 
-    public static void main(String[] args) {
-        Range range = new Range();
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.print("Введите начало числового диапазона: ");
-        range.setFrom(scanner.nextDouble());
-
-        System.out.print("Введите конец числового диапазона: ");
-        range.setTo(scanner.nextDouble());
-
-        System.out.print("Введите число которое хотите проверить: ");
-        double x = scanner.nextDouble();
-
-        System.out.printf("Длина числовго диапазона от %.2f до %.2f = %.2f%n", range.getFrom(), range.getTo(), range.getLength());
-
-        if (range.isInside(x)) {
-            System.out.printf("Число %.2f принадлежит диапазону", x);
-        } else {
-            System.out.printf("Число %.2f не принадлежит диапазону", x);
-        }
-    }
-
-    public final double epsilon = 1.0e-10;
-    public double from;
-    public double to;
-
-    public Range() {
+    public Range(double from, double to) {
         this.from = from;
         this.to = to;
     }
@@ -39,23 +14,25 @@ public class Range {
         return from;
     }
 
-    public void setFrom(double from) {
-        this.from = from;
+    public double setFrom(double from) {
+        from = from;
+        return from;
     }
 
     public double getTo() {
         return to;
     }
 
-    public void setTo(double to) {
-        this.to = to;
+    public double setTo(double to) {
+        to = to;
+        return to;
     }
 
     public double getLength() {
         return to - from;
     }
 
-    public boolean isInside(double x) {
-        return x - from > -epsilon && x - to < epsilon;
+    public boolean isInside(double number) {
+        return number - from > -EPSILON && number - to < EPSILON;
     }
 }
