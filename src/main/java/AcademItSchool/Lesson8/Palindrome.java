4a5f92907ec0568dc7d81d8dff921f73049162a3
Valery0904 <valery.tugolukov@gmail.com>
2024-03-03 10:22:50 +0700

    Задача на курс «Палиндром»
    2. countMonthsInYear - неправильный порядок слов.
    На английском нужно указывать сначала "чего"/"чье" (месяцев в году), а потом "что" (количество)
    
    3. Не везде исправлен п.2

---
+++ src/main/java/AcademItSchool/Lesson8/Palindrome.java
@@ -0,0 +1,40 @@
+package AcademItSchool.Lesson8;
+
+import java.util.Scanner;
+
+public class Palindrome {
+    public static boolean isPalindrome(String string) {
+
+        for (int i = 0, j = string.length() - 1; i < j; i++, j--) {
+            if(!Character.isLetter(string.charAt(i))) {
+                j++;
+                continue;
+            }
+
+            if (!Character.isLetter(string.charAt(j))){
+                i--;
+                continue;
+            }
+
+            if (string.charAt(i) != string.charAt(j)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Введите строку: ");
+        String string = scanner.nextLine().toLowerCase();
+
+        boolean stringIsPalindrome = isPalindrome(string);
+
+        if (stringIsPalindrome) {
+            System.out.println("Строка является палиндромом.");
+        } else {
+            System.out.println("Строка не является палиндромом.");
+        }
+    }
+}
