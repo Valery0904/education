8875d76fab22ac2db667044b126433725ce8aaff
Valery0904 <valery.tugolukov@gmail.com>
2024-03-03 09:32:24 +0700

    Курсовая "Алгоритм Евклида"
     Вместо first, second и др. в именах лучше использовать цифры

--- src/main/java/AcademItSchool/Coursework/EuclideanAlgorithm.java
+++ src/main/java/AcademItSchool/Coursework/EuclideanAlgorithm.java
@@ -3,29 +3,29 @@ package AcademItSchool.Coursework;
 import java.util.Scanner;
 
 public class EuclideanAlgorithm {
-    public static int getGreatestCommonDivisor(int firstNumber, int secondNumber) {
-        while (secondNumber != 0) {
-            int temp = firstNumber % secondNumber;
-            firstNumber = secondNumber;
-            secondNumber = temp;
+    public static int getGreatestCommonDivisor(int number1, int number2) {
+        while (number2 != 0) {
+            int temp = number1 % number2;
+            number1 = number2;
+            number2 = temp;
         }
 
-        return firstNumber;
+        return number1;
     }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         System.out.print("Введите первое число: ");
-        int firstNumber = scanner.nextInt();
+        int number1 = scanner.nextInt();
 
         System.out.print("Введите второе число: ");
-        int secondNumber = scanner.nextInt();
+        int number2 = scanner.nextInt();
 
-        if (firstNumber == 0 && secondNumber == 0) {
+        if (number1 == 0 && number2 == 0) {
             System.out.println("Оба числа равны нулю, НОД искать нельзя");
         } else {
-            System.out.printf("НОД: %d", getGreatestCommonDivisor(firstNumber, secondNumber));
+            System.out.printf("НОД: %d", getGreatestCommonDivisor(number1, number2));
         }
     }
 }
