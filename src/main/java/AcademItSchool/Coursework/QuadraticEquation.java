62a743f390a16a5a2f7a188228c027b0c03ba08d
Valery0904 <valery.tugolukov@gmail.com>
2024-03-04 22:24:41 +0700

    Курсовая "Квадратное уравнение"
    1. Есть ошибки в некоторых проверках через epsilon
    
    2. "Коренем" - опечатка
    
    3. Не исправлен п.4
    
    4. Есть лишние случаи, которые получатся через другие случаи.
    Эти случаи нужно удалить

--- src/main/java/AcademItSchool/Coursework/QuadraticEquation.java
+++ src/main/java/AcademItSchool/Coursework/QuadraticEquation.java
@@ -15,39 +15,31 @@ public class QuadraticEquation {
         System.out.print("Введите коэффициент C: ");
         double c = scanner.nextDouble();
 
-        final double epsilon = 1.0e-10;
+        final double EPSILON = 1.0e-10;
 
-        if (Math.abs(a) <= epsilon && Math.abs(b) <= epsilon) {
-            System.out.println("Коренем является любое число");
-        } else if (Math.abs(b) <= epsilon && Math.abs(c) <= epsilon) {
-            System.out.println("Корень x = 0");
-        } else if (Math.abs(a) <= epsilon) {
-            double x = -c / b;
-            System.out.printf("Корень x = %.2f", x);
-        } else if (Math.abs(c) <= epsilon) {
-            double x1 = 0;
-            double x2 = -b / a;
-            System.out.printf("Корни:%nx1 = %.2f%nx2 = %.2f", x1, x2);
-        } else if (Math.abs(b) <= epsilon) {
-            if ((-c / a) <= epsilon) {
-                System.out.println("Корней нет");
+        if (Math.abs(a) < EPSILON) {
+            if (Math.abs(b) < EPSILON) {
+                if (Math.abs(c) < EPSILON) {
+                    System.out.println("Уравнение имеет множество решений");
+                } else {
+                    System.out.println("Уравнение не имеет решения");
+                }
             } else {
-                double x1 = Math.sqrt(-c / a);
-                double x2 = -Math.sqrt(-c / a);
-                System.out.printf("Корни:%nx1 = %.2f%nx2 = %.2f", x1, x2);
+                double x = -c / b;
+                System.out.printf("Корень x = %.2f", x);
             }
         } else {
             double discriminant = Math.pow(b, 2) - 4 * a * c;
 
-            if (discriminant < epsilon) {
-                System.out.println("Корней нет!");
-            } else if (discriminant == epsilon) {
+            if (Math.abs(discriminant) < EPSILON) {
                 double x = -b / (2 * a);
                 System.out.printf("Корень x = %.2f", x);
-            } else {
+            } else if (discriminant > 0) {
                 double x1 = (-b + Math.sqrt(discriminant)) / (2 * a);
                 double x2 = (-b - Math.sqrt(discriminant)) / (2 * a);
                 System.out.printf("Корень x1 = %.2f%nКорень x2 = %.2f", x1, x2);
+            } else {
+                System.out.println("Корней нет!");
             }
         }
     }
