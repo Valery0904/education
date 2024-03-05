6f16f0512b1acdb005a12b20243cf3d1a025e3b5
Valery0904 <valery.tugolukov@gmail.com>
2024-03-05 21:56:55 +0700

    «Разворот массива»
    1. Вместо i <= array.length - 1 лучше писать i < array.length
    
    2. По заданию функция должна менять переданный массив
    
    3. Текст для пользователя неправильный по смыслу.
    И лучше начинать его с заглавной буквы
    
    4. arrayLength - имя не подходит по смыслу

--- src/main/java/AcademItSchool/Lesson9/InvertedArray.java
+++ src/main/java/AcademItSchool/Lesson9/InvertedArray.java
@@ -4,20 +4,17 @@ import java.util.Arrays;
 
 public class InvertedArray {
     public static int[] getInvertedArray(int[] array) {
-        int[] invertedArray = new int[array.length];
-        int arrayLength = array.length - 1;
-
-        for (int i = 0; i <= array.length - 1; i++) {
-            invertedArray[i] = array[arrayLength];
-            arrayLength--;
+        for (int i = 0, j = array.length - 1; i < array.length / 2; i++, j--) {
+            int temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
         }
 
-        return invertedArray;
+        return array;
     }
 
     public static void main(String[] args) {
         int[] array = {10, 9, 18, 11, 5, 36, 12, 52, 4, 1, 99};
-
-        System.out.println("максимальное число в массиве = " + Arrays.toString(getInvertedArray(array)));
+        System.out.println(Arrays.toString(getInvertedArray(array)));
     }
 }
