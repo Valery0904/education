a4b71926a4bd24324e95aeeb4f48b0e4f12af29e
Valery0904 <valery.tugolukov@gmail.com>
2024-03-10 21:22:09 +0700

    «Перевод файла в верхний регистр»
    Написать программу, которая читает строки файла,
    переводит их в верхний регистр и записывает
    результат во второй файл

---
+++ src/main/java/AcademItSchool/Lesson12/ConvertFileToUpperCase.java
@@ -0,0 +1,18 @@
+package AcademItSchool.Lesson12;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class ConvertFileToUpperCase {
+    public static void main(String[] args) throws FileNotFoundException {
+        try (Scanner scanner = new Scanner(new FileInputStream("input.txt"));
+             PrintWriter writer = new PrintWriter("output.txt")) {
+            while (scanner.hasNextLine()) {
+                String string = scanner.nextLine();
+                writer.println(string.toUpperCase());
+            }
+        }
+    }
+}
