635d1da3291b33c3289f1d80af61d08400d32876
Valery0904 <valery.tugolukov@gmail.com>
2024-03-11 19:20:11 +0700

    «Разворот массива»
    1. Эта функция не создает новый массив, а меняет переданный.
    Поэтому по смыслу эта функция должна быть void

--- src/main/java/AcademItSchool/Lesson9/InvertedArray.java
+++ src/main/java/AcademItSchool/Lesson9/InvertedArray.java
@@ -3,7 +3,7 @@ package AcademItSchool.Lesson9;
 import java.util.Arrays;
 
 public class InvertedArray {
-    public static int[] getInvertedArray(int[] array) {
+    public static void getInvertedArray(int[] array) {
         int arrayHalf = array.length / 2;
 
         for (int i = 0, j = array.length - 1; i < arrayHalf; i++, j--) {
@@ -11,12 +11,13 @@ public class InvertedArray {
             array[i] = array[j];
             array[j] = temp;
         }
-
-        return array;
     }
 
     public static void main(String[] args) {
         int[] array = {10, 9, 18, 11, 5, 36, 12, 52, 4, 1, 99};
-        System.out.println(Arrays.toString(getInvertedArray(array)));
+
+        getInvertedArray(array);
+
+        System.out.println(Arrays.toString(array));
     }
 }
