6dbb64e9907e5832aae6839662a9e990b8190915
Valery0904 <valery.tugolukov@gmail.com>
2024-03-11 19:50:35 +0700

    Курсовая «Сортировка выбором»
    1. Нужно уменьшить количество итераций во внешнем цикле

--- src/main/java/AcademItSchool/Lesson11/SelectionSort.java
+++ src/main/java/AcademItSchool/Lesson11/SelectionSort.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class SelectionSort {
     public static void selectionSort(int[] array) {
-        for (int i = 0; i < array.length; i++) {
+        for (int i = 0; i < array.length - 1; i++) {
             int minElementIndex = i;
 
             for (int j = i + 1; j < array.length; j++) {
