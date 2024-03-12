239e3a84b802e5e9c6a72958c14727af151275e7
Valery0904 <valery.tugolukov@gmail.com>
2024-03-12 20:07:25 +0700

    «Массив строк в верх. рег»
    1. Если функция void, имя не должно начинаться с get, т.к. функция ничего не выдает

--- src/main/java/AcademItSchool/Lesson9/StringsArrayInUpperCase.java
+++ src/main/java/AcademItSchool/Lesson9/StringsArrayInUpperCase.java
@@ -3,7 +3,7 @@ package AcademItSchool.Lesson9;
 import java.util.Arrays;
 
 public class StringsArrayInUpperCase {
-    public static void getStringsArrayInUpperCase(String[] stringsArray) {
+    public static void stringsArrayInUpperCase(String[] stringsArray) {
         for (int i = 0; i < stringsArray.length; i++) {
             stringsArray[i] = stringsArray[i].toUpperCase();
         }
@@ -12,7 +12,7 @@ public class StringsArrayInUpperCase {
     public static void main(String[] args) {
         String[] stringsArray = {"aaa", "bbb", "ccc"};
 
-        getStringsArrayInUpperCase(stringsArray);
+        stringsArrayInUpperCase(stringsArray);
 
         System.out.println(Arrays.toString(stringsArray));
     }
