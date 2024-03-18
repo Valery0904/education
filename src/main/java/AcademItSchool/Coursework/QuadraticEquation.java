b5b273cb73bebf8fb9ba9aa3f325ee7f8c3b432e
Valery0904 <valery.tugolukov@gmail.com>
2024-03-18 21:41:55 +0700

    Курсовая "Квадратное уравнение"
    1. Сейчас проверки через epsilon не согласованы между собой.
    Если в проверке равенства было строгое неравенство, то в проверках, что значения не равны, должны быть нестрогие неравенства

--- src/main/java/AcademItSchool/Coursework/QuadraticEquation.java
+++ src/main/java/AcademItSchool/Coursework/QuadraticEquation.java
@@ -17,9 +17,9 @@ public class QuadraticEquation {
 
         final double EPSILON = 1.0e-10;
 
-        if (Math.abs(a) < EPSILON) {
-            if (Math.abs(b) < EPSILON) {
-                if (Math.abs(c) < EPSILON) {
+        if (Math.abs(a) <= EPSILON) {
+            if (Math.abs(b) <= EPSILON) {
+                if (Math.abs(c) <= EPSILON) {
                     System.out.println("Уравнение имеет бесконечное число решений");
                 } else {
                     System.out.println("Уравнение не имеет решения");
@@ -31,10 +31,10 @@ public class QuadraticEquation {
         } else {
             double discriminant = Math.pow(b, 2) - 4 * a * c;
 
-            if (Math.abs(discriminant) < EPSILON) {
+            if (Math.abs(discriminant) <= EPSILON) {
                 double x = -b / (2 * a);
                 System.out.printf("Корень x = %.2f", x);
-            } else if (discriminant > EPSILON) {
+            } else if (discriminant >= EPSILON) {
                 double x1 = (-b + Math.sqrt(discriminant)) / (2 * a);
                 double x2 = (-b - Math.sqrt(discriminant)) / (2 * a);
                 System.out.printf("Корень x1 = %.2f%nКорень x2 = %.2f", x1, x2);
