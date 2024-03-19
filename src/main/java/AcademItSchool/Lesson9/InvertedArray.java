d84c17cb0aeefbd1c6d13d51bad348724c416a92
Valery0904 <valery.tugolukov@gmail.com>
2024-03-19 21:20:42 +0700

    «Разворот массива»
    1. Имена функций должны начинаться с глагола
    
    2. arrayHalf - в имени не хватает слова "длина"

--- src/main/java/AcademItSchool/Lesson9/InvertedArray.java
+++ src/main/java/AcademItSchool/Lesson9/InvertedArray.java
@@ -3,10 +3,10 @@ package AcademItSchool.Lesson9;
 import java.util.Arrays;
 
 public class InvertedArray {
-    public static void invertedArray(int[] array) {
-        int arrayHalf = array.length / 2;
+    public static void invertArray(int[] array) {
+        int arrayHalfLength = array.length / 2;
 
-        for (int i = 0, j = array.length - 1; i < arrayHalf; i++, j--) {
+        for (int i = 0, j = array.length - 1; i < arrayHalfLength; i++, j--) {
             int temp = array[i];
             array[i] = array[j];
             array[j] = temp;
@@ -16,7 +16,7 @@ public class InvertedArray {
     public static void main(String[] args) {
         int[] array = {10, 9, 18, 11, 5, 36, 12, 52, 4, 1, 99};
 
-        invertedArray(array);
+        invertArray(array);
 
         System.out.println(Arrays.toString(array));
     }
