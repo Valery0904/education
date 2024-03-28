c27f9de7c3aab0de43c9cb3492104faad3c15da3
Valery0904 <valery.tugolukov@gmail.com>
2024-03-28 19:59:43 +0700

    Курсовая "Квадратное уравнение"
    1. http://joxi.ru/a2X6NQ6HZ4Oa7r - это проверка на равенство, поэтому здесь, как и в остальных проверках на равенство, должно быть нестрогое неравенство

--- src/main/java/AcademItSchool/Coursework/QuadraticEquation.java
+++ src/main/java/AcademItSchool/Coursework/QuadraticEquation.java
@@ -31,7 +31,7 @@ public class QuadraticEquation {
         } else {
             double discriminant = Math.pow(b, 2) - 4 * a * c;
 
-            if (Math.abs(discriminant) < EPSILON) {
+            if (Math.abs(discriminant) <= EPSILON) {
                 double x = -b / (2 * a);
                 System.out.printf("Корень x = %.2f", x);
             } else if (discriminant > EPSILON) {
