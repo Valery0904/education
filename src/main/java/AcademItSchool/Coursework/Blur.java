32cf1b8e585853f678cc143d19581f3d50052533
Valery0904 <valery.tugolukov@gmail.com>
2024-04-04 22:53:44 +0700

    Курсовая «Размытие»
    1. Нужно отделять цепочки if'ов от остального кода:
    http://joxi.ru/zAN5ZRVIxOWln2
    
    2. saturate(double value) - аргументу нужно дать более информативное имя
    
    3. matrixWidth, matrixHeight:
    - слова высота и ширина не подходят для матрицы.
    Нужно говорить о количестве строк и столбцов
    - матрица должна быть квадратной, поэтому достаточно одной переменной "размер матрицы"
    
    4. new double[3][3] - лучше наоборот сделать переменную "размер матрицы" и через нее построить матрицу
    
    5. 1d / 9 - значение выражения заново вычисляется с одинаковыми аргументами на каждой итерации цикла.
    В таком случае лучше завести переменную и вычислить ее значение до начала цикла
    
    6. 1d / 9 - это значение нужно вычислить через размер матрицы
    
    7. for (double[] convolutionCoefficient : matrix) - имя convolutionCoefficient неправильное, по смыслу это строка матрицы
    
    8. final double COLORS_COUNT_IN_RGB = 3.0; - эта константа не должна быть double, т.к. количество по смыслу является целым числом
    
    9. pixelSum - здесь слово пиксель должно быть в конце имени
    
    10. matrixHeight / 2, height - matrixHeight / 2, matrixWidth / 2, width - matrixWidth / 2 - значение выражения заново вычисляется с одинаковыми аргументами на каждой итерации цикла.
    В таком случае лучше завести переменную и вычислить ее значение до начала цикла
    
    11. В циклах по i и j цикл по координате, соответствующей оси y, должен быть внешним циклом, это работает быстрее
    
    12. Сейчас перепутаны оси x и y.
    По смыслу в матрице первое измерение отвечает за строки (т.е. ось y), а второе за столбцы (т.е. ось x)
    
    13. В последнем цикле по k свободно имя i

--- src/main/java/AcademItSchool/Coursework/Blur.java
+++ src/main/java/AcademItSchool/Coursework/Blur.java
@@ -8,15 +8,16 @@ import java.io.IOException;
 import java.util.Arrays;
 
 public class Blur {
-    public static int saturate(double value) {
-        if (value <= 0) {
+    public static int saturate(double colorIntensity) {
+        if (colorIntensity <= 0) {
             return 0;
         }
 
-        if (value >= 255) {
+        if (colorIntensity >= 255) {
             return 255;
         }
-        return (int) Math.round(value);
+
+        return (int) Math.round(colorIntensity);
     }
 
     public static void main(String[] args) throws IOException {
@@ -29,41 +30,43 @@ public class Blur {
         BufferedImage outputImage = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
         WritableRaster outputRaster = outputImage.getRaster();
 
-        double[][] matrix = new double[3][3];
+        int size = 3;
+        double[][] matrix = new double[size][size];
+        int matrixSize = matrix.length;
+        double blurCoefficient = 1 / Math.pow(matrixSize, 2);
 
-        for (double[] convolutionCoefficient : matrix) {
-            Arrays.fill(convolutionCoefficient, 1d / 9);
+        for (double[] matrixLine : matrix) {
+            Arrays.fill(matrixLine, blurCoefficient);
         }
 
-        int matrixWidth = matrix.length;
-        int matrixHeight = matrix[0].length;
+        final int COLORS_COUNT_IN_RGB = 3;
 
-        final double COLORS_COUNT_IN_RGB = 3.0;
+        int[] pixel = new int[COLORS_COUNT_IN_RGB];
+        double[] sumPixels = new double[COLORS_COUNT_IN_RGB];
 
-        int[] pixel = new int[(int) COLORS_COUNT_IN_RGB];
-        double[] pixelSum = new double[(int) COLORS_COUNT_IN_RGB];
+        int coefficientForSibling = matrixSize / 2;
 
-        for (int y = matrixHeight / 2; y < height - matrixHeight / 2; ++y) {
-            for (int x = matrixWidth / 2; x < width - matrixWidth / 2; ++x) {
-                pixelSum[0] = 0;
-                pixelSum[1] = 0;
-                pixelSum[2] = 0;
+        for (int y = coefficientForSibling; y < width - coefficientForSibling; ++y) {
+            for (int x = coefficientForSibling; x < height - coefficientForSibling; ++x) {
+                sumPixels[0] = 0;
+                sumPixels[1] = 0;
+                sumPixels[2] = 0;
 
-                for (int i = -matrixWidth / 2; i <= matrixWidth / 2; ++i) {
-                    for (int j = -matrixHeight / 2; j <= matrixHeight / 2; ++j) {
-                        inputRaster.getPixel(x + i, y + j, pixel);
+                for (int i = -coefficientForSibling; i <= coefficientForSibling; ++i) {
+                    for (int j = -coefficientForSibling; j <= coefficientForSibling; ++j) {
+                        inputRaster.getPixel(y + i, x + j, pixel);
 
                         for (int k = 0; k < COLORS_COUNT_IN_RGB; k++) {
-                            pixelSum[k] += pixel[k] * matrix[i + matrixWidth / 2][j + matrixHeight / 2];
+                            sumPixels[k] += pixel[k] * matrix[i + coefficientForSibling][j + coefficientForSibling];
                         }
                     }
                 }
 
-                for (int k = 0; k < COLORS_COUNT_IN_RGB; k++) {
-                    pixelSum[k] = saturate(pixelSum[k]);
+                for (int i = 0; i < COLORS_COUNT_IN_RGB; i++) {
+                    sumPixels[i] = saturate(sumPixels[i]);
                 }
 
-                outputRaster.setPixel(x, y, pixelSum);
+                outputRaster.setPixel(y, x, sumPixels);
             }
         }
 
