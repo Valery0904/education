c939bd8e584f0083d5ad03059c65b37699c07ea4
Valery0904 <valery.tugolukov@gmail.com>
2024-04-08 21:40:27 +0700

    Курсовая «Размытие»
    1. http://joxi.ru/2Y0DYaoi0xVX4m - перепутаны координаты x и y.
    Сейчас код не падает только потому что одновременно с этим перепутаны размеры картинки:
    http://joxi.ru/2Q4dWQlc4O3Ygm
    
    2. http://joxi.ru/mdn8GY6cJX1jQA - не нужны сразу две переменные для размера матрицы.
    Текущую matrixSize нужно удалить, а size переименовать в matrixSize
    
    3. matrixLine - для строки матрицы вместо слова line нужно использовать слово row
    
    4. sumPixels:
    - лишнее множественное число в имени
    - лучше назвать "выходной пиксель"
    
    5. coefficientForSibling - неинформативное имя.
    И это не коэффициент, т.е. не множитель.
    Можно назвать, например, "половина размера матрицы" или "отступ"
    
    6. width - coefficientForSibling, height - coefficientForSibling - значение выражения заново вычисляется с одинаковыми аргументами на каждой итерации цикла.
    В таком случае лучше завести переменную и вычислить ее значение до начала цикла
    
    7. y + i, i + coefficientForSibling - значения выражений заново вычисляются с одинаковыми аргументами на каждой итерации цикла по j.
    Эти значения лучше вычислять в цикле по i
    
    8. y + i, i + coefficientForSibling, x + j, j + coefficientForSibling - вместо вычисления этих выражений лучше в соответствующих for вместо счетчиков i и j завести по два счетчика: для координаты соседнего пикселя и для индекса матрицы
    
    9. Лучше добавить пустую строку для читаемости:
    http://joxi.ru/n2Yz7N5TVp3ovA

--- src/main/java/AcademItSchool/Coursework/Blur.java
+++ src/main/java/AcademItSchool/Coursework/Blur.java
@@ -30,43 +30,45 @@ public class Blur {
         BufferedImage outputImage = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
         WritableRaster outputRaster = outputImage.getRaster();
 
-        int size = 3;
-        double[][] matrix = new double[size][size];
-        int matrixSize = matrix.length;
+        int matrixSize = 3;
+        double[][] matrix = new double[matrixSize][matrixSize];
+
         double blurCoefficient = 1 / Math.pow(matrixSize, 2);
 
-        for (double[] matrixLine : matrix) {
-            Arrays.fill(matrixLine, blurCoefficient);
+        for (double[] matrixRow : matrix) {
+            Arrays.fill(matrixRow, blurCoefficient);
         }
 
         final int COLORS_COUNT_IN_RGB = 3;
 
         int[] pixel = new int[COLORS_COUNT_IN_RGB];
-        double[] sumPixels = new double[COLORS_COUNT_IN_RGB];
+        double[] outputPixel = new double[COLORS_COUNT_IN_RGB];
 
-        int coefficientForSibling = matrixSize / 2;
+        int indentation = matrixSize / 2;
+        int widthIndent = width - indentation;
+        int heightIndent = height - indentation;
 
-        for (int y = coefficientForSibling; y < width - coefficientForSibling; ++y) {
-            for (int x = coefficientForSibling; x < height - coefficientForSibling; ++x) {
-                sumPixels[0] = 0;
-                sumPixels[1] = 0;
-                sumPixels[2] = 0;
+        for (int y = indentation; y < heightIndent; ++y) {
+            for (int x = indentation; x < widthIndent; ++x) {
+                outputPixel[0] = 0;
+                outputPixel[1] = 0;
+                outputPixel[2] = 0;
 
-                for (int i = -coefficientForSibling; i <= coefficientForSibling; ++i) {
-                    for (int j = -coefficientForSibling; j <= coefficientForSibling; ++j) {
-                        inputRaster.getPixel(y + i, x + j, pixel);
+                for (int i = 0, pixelXPosition = x - indentation; i < matrixSize; ++i, ++pixelXPosition) {
+                    for (int j = 0, pixelYPosition = y - indentation; j < matrixSize; ++j, ++pixelYPosition) {
+                        inputRaster.getPixel(pixelXPosition, pixelYPosition, pixel);
 
                         for (int k = 0; k < COLORS_COUNT_IN_RGB; k++) {
-                            sumPixels[k] += pixel[k] * matrix[i + coefficientForSibling][j + coefficientForSibling];
+                            outputPixel[k] += pixel[k] * matrix[i][j];
                         }
                     }
                 }
 
                 for (int i = 0; i < COLORS_COUNT_IN_RGB; i++) {
-                    sumPixels[i] = saturate(sumPixels[i]);
+                    outputPixel[i] = saturate(outputPixel[i]);
                 }
 
-                outputRaster.setPixel(y, x, sumPixels);
+                outputRaster.setPixel(x, y, outputPixel);
             }
         }
 
